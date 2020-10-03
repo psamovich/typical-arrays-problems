fb019ebc0010ebd446673d34f2e14e5181b500d7
Pavel Samovich <pavel.samovich@gmail.com>
2020-10-03 16:53:59 +0300

    feat: implement min, max and avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
+exports.min = function min(array) {
+    if (isNotValidParam(array)) {
+        return 0;
+    }
+    return array.reduce((min, current) => Math.min(min, current));
+};
 
-exports.min = function min (array) {
-  return 0;
+exports.max = function max(array) {
+    if (isNotValidParam(array)) {
+        return 0;
+    }
+    return array.reduce((max, current) => Math.max(max, current));
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (isNotValidParam(array)) {
+        return 0;
+    }
+    return array.reduce((sum, current) => sum += current) / array.length;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+function isNotValidParam(array) {
+    return !array || array.length === 0;
 }
